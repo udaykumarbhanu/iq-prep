1813a3d1f3b39d15391515f200a0e8b6c1244502
udaykumarbhanu@gmail.com <udaykumarbhanu@gmail.com>
2019-03-06 12:00:38 +0530

    plus-one to number in array which may have zero at start

---
+++ ibts364/plus-one-array-may-have-zero-at-start.py
@@ -0,0 +1,47 @@
+'''Given a non-negative number represented as an array of digits,
+
+add 1 to the number ( increment the number represented by the digits ).
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Example:
+
+If the vector has [1, 2, 3]
+
+the returned vector should be [1, 2, 4]
+
+as 123 + 1 = 124.
+'''
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+
+    def plusOne(self, A):
+        #Case0: [0, 0, 0, 9]
+        while A[0] == 0 and len(A)>1:
+            del A[0]
+
+        if A[0] == 0 and len(A) == 1:
+            return A[1]
+
+        n = len(A)
+
+        for index in range(n-1, -1, -1):
+            if A[index] < 9:
+                A[index] += 1
+
+                # Case1: [1, 2, 3]
+                return A
+            else:
+                # Case2: [1, 2, 9]
+                A[index] = 0
+
+        # Case3: [9, 9, 9]
+        A.insert(0, 1)
+
+        return A
+
+if __name__ == '__main__':
+    A = [0, 0, 9]
+    print Solution().plusOne(A)
