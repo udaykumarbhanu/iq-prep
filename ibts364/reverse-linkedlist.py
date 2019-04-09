2d24c5c4b4095eb83e8646d5c51735aac2a37662
udaykumarbhanu@gmail.com <udaykumarbhanu@gmail.com>
2019-04-09 20:18:59 +0530

    reverse-linkedlist.py

--- ibts364/reverse-linkedlist.py
+++ ibts364/reverse-linkedlist.py
@@ -1,15 +1,15 @@
-'''Given a linked list, determine if it has a cycle in it.
+'''Reverse a singly linked list.
 
-To represent a cycle in the given linked list, we use an integer pos which
-represents the position (0-indexed) in the linked list where tail connects to.
-If pos is -1, then there is no cycle in the linked list.
+Example:
 
-Example 1:
-
-Input: head = [3,2,0,-4], pos = 1
-Output: true
-Explanation: There is a cycle in the linked list, where tail connects to the second node.
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
 '''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 # Definition for singly-linked list.
 # class ListNode(object):
@@ -18,10 +18,10 @@ Explanation: There is a cycle in the linked list, where tail connects to the sec
 #         self.next = None
 
 class Solution(object):
-    def hasCycle(self, head):
+    def reverseList(self, head):
         """
         :type head: ListNode
-        :rtype: bool
+        :rtype: ListNode
         """
         if not head:
             return head
