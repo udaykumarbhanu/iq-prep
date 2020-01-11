fa6c3b984f42e6d53dcb7cae38cba937c1b4cf6b
udaykumarbhanu@gmail.com <udaykumarbhanu@gmail.com>
2020-01-12 00:23:57 +0530

    numbers_with_equal_digit_sum.py

---
+++ msft-oa/numbers_with_equal_digit_sum.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/discuss/interview-question/365872/
+
+def numbers_with_equal_digit_sum(nums):
+    if not nums:
+        return -1
+
+    table = dict()
+    result = -1
+
+    for num in nums:
+        total = 0
+        num_copy = num
+        while num > 0:
+            total += num % 10
+            num /= 10
+
+        if total not in table:
+            table[total] = num_copy
+        else:
+            result = max(result, table[total] + num_copy)
+            table[total] = max(num_copy, table[total])
+
+    return result
+
+
+if __name__ == '__main__':
+    A = [51, 71, 17, 42]
+    print numbers_with_equal_digit_sum(A)
