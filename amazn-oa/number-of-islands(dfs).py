1d161f84162824fc07d0c6561deeeb3d3022fb0c
udaykumarbhanu@gmail.com <udaykumarbhanu@gmail.com>
2020-01-19 19:50:46 +0530

    number-of-islands(dfs).py solution using dfs.

---
+++ amazn-oa/number-of-islands(dfs).py
@@ -0,0 +1,59 @@
+'''
+200. Number of Islands
+https://leetcode.com/problems/number-of-islands/
+
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+
+Input:
+11110
+11010
+11000
+00000
+
+Output: 1
+Example 2:
+
+Input:
+11000
+11000
+00100
+00011
+
+Output: 3
+'''
+
+
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid: return 0
+
+        result = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    result += self.dfs(grid, i, j)
+
+        return result
+
+    def dfs(self, grid, row, col):
+        if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or grid[row][col] == '0':
+            return 0
+
+        grid[row][col] = '0'
+        self.dfs(grid, row + 1, col)
+        self.dfs(grid, row - 1, col)
+        self.dfs(grid, row, col + 1)
+        self.dfs(grid, row, col - 1)
+
+        return 1
+
+if __name__ == '__main__':
+    grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+
+    print Solution().numIslands(grid)
